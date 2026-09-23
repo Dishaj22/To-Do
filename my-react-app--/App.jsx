65ff96098d8fddc34ad4468ede6f94039d6c4125
Dishaj22 <dishajaiswal2301@gmail.com>
2026-09-23 16:55:51 +0530

    Add files via upload

---
+++ my-react-app--/App.jsx
@@ -0,0 +1,33 @@
+// import DocumentTitleSync from './component/DocumentTitlesSync'
+
+// function App(){} 
+//   return (
+//     <div>
+//       <DocumentTitleSync/>
+//     </div>
+//   )
+
+
+// export default App;
+
+// import React from 'react'
+// import Dashborad from './component/Dashborad'
+
+// const App = () => {
+//   return (
+//     <div>Dashborad</div>
+//   )
+// }
+
+// export default App
+import UserList from "./component/UserList";
+
+function App() {
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app--/UserList.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+
+      const data = await response.json();
+
+      setUsers(data);
+    }
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h2>User List</h2>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
